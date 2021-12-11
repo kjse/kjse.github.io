a7c5e561a8333a800d790423d7cc03662aad68a6
kjse <75707747+kjse@users.noreply.github.com>
2021-12-10 18:53:23 -0800

    Create fancifymytext.js

---
+++ fancifymytext.js
@@ -0,0 +1,24 @@
+function biggerCall(){
+alert("Hello, World!");
+document.getElementById("text").style.fontSize="24pt";
+}
+
+function fancifyCall(){
+   if(document.getElementById("fancy").checked){
+    document.getElementById("text").style.fontWeight="bold";
+    document.getElementById("text").style.color="blue";
+    document.getElementById("text").style.textDecoration="underline";
+   }
+   else{
+    document.getElementById("text").style.fontWeight="normal";
+    document.getElementById("text").style.color="black";
+    document.getElementById("text").style.textDecoration="none";
+   }
+}
+
+function mooCall(){
+  alert("moo");
+  var str = document.getElementById("text");
+  var parts = str.value.split(" ");
+  str.value = parts.join("-Moo.");
+}
